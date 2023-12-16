5c09e86d706a1efc321acea718e262dc7e108782
MarvinVN <marvinvnguyen@gmail.com>
2023-12-15 23:54:08 -0500

    first commit

---
+++ src/test/java/com/hbp/acme_learning/AcmeLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbp.acme_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
